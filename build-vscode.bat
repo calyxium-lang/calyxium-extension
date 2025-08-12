49c210ac8268ab81dcdefffaba7e4b113388fcc1
Mr-Zombii <thezombiigames@gmail.com>
2025-08-12 13:20:18 -0500

    remove buildscripts as they are not needed :)

--- build-vscode.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-mkdir bin
-cd vscode
-vsce package -o ../bin/calyxium-lsp.vsix
-cd ../
--- build.ps1
+++
@@ -1,2 +0,0 @@
-New-Item bin -Type Directory -Force | Out-Null
-vsce package -o "bin/calyxium-lsp.vsix" -C vscode
